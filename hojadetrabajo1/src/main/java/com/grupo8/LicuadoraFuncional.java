b76280987b917bbf2cbb8d83937084413f2c8196
RodrigoAar <ajmacrodrigo@gmail.com>
2025-01-26 22:56:18 -0600

    Update LicuadoraFuncional.java
    Definir y agregar los atributos de la clase. Crear el constructor de la misma.

--- hojadetrabajo1/src/main/java/com/grupo8/LicuadoraFuncional.java
+++ hojadetrabajo1/src/main/java/com/grupo8/LicuadoraFuncional.java
@@ -1,5 +1,14 @@
 package com.grupo8;
 
 public class LicuadoraFuncional implements Licuadora{
+    private boolean encendida;
+    private double volumenactual;
+    private int velocidad;
+
+    public LicuadoraFuncional(){
+        this.encendida = false;
+        this.velocidad = 0;
+        this.volumenactual = 0;
+    };
 
 }
