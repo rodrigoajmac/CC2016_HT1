ed7ce3078168ae92dca88e3685d32d11f6c77c7a
RodrigoAar <ajmacrodrigo@gmail.com>
2025-01-26 22:57:34 -0600

    Update LicuadoraFuncional.java
    Agregar un atributo de capacidad máxima, este debería ser definido por el programador.

--- hojadetrabajo1/src/main/java/com/grupo8/LicuadoraFuncional.java
+++ hojadetrabajo1/src/main/java/com/grupo8/LicuadoraFuncional.java
@@ -4,11 +4,14 @@ public class LicuadoraFuncional implements Licuadora{
     private boolean encendida;
     private double volumenactual;
     private int velocidad;
+    private double capacidadMaxima = 2.0;
 
     public LicuadoraFuncional(){
         this.encendida = false;
         this.velocidad = 0;
         this.volumenactual = 0;
     };
+
+
 
 }
